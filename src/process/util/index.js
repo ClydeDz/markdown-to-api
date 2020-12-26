3d2c7ff9cba74e730b6a25754b7cc5433dc89978
Clyde D'Souza <clyd94@gmail.com>
2020-12-27 11:44:27 +1300

    Add validation, constants and use JSON properties instead of variables

--- src/process/util/index.js
+++ src/process/util/index.js
@@ -1,4 +1,5 @@
 const configFileSchema = require("./schema");
+const constants = require("./constants");
 
 function validateJSON(inputJSON){
     try{
@@ -19,5 +20,26 @@ function validateConfigFile(configFile){
     }
 }
 
+function isInputFileFilterValid(inputFileFilter) {
+    !constants.VALID_FILE_EXTENSIONS.includes(inputFileFilter);
+}
+
+function sanitizeConfigFile(configFile) {
+    if(!configFile.output){
+        configFile.output = constants.DEFAULT_OUTPUT_DIRECTORY;
+    }
+    if(!configFile.summaryOutput){
+        configFile.summaryOutput = constants.DEFAULT_SUMMARY_OUTPUT_DIRECTORY;
+    }
+    if(!configFile.filename){
+        configFile.filename = constants.DEFAULT_SUMMARY_FILENAME;
+    }
+    if(!configFile.inputFileFilter || isInputFileFilterValid(configFile.inputFileFilter)) {
+        configFile.inputFileFilter = constants.VALID_FILE_EXTENSIONS[0];
+    } 
+    return configFile;
+}
+
 exports.validateJSON = validateJSON;
 exports.validateConfigFile = validateConfigFile;
+exports.sanitizeConfigFile = sanitizeConfigFile;
